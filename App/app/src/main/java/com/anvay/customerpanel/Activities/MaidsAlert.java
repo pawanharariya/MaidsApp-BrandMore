17ddc688873f1191d746aafe18a7a1a29cd97f78
pawanharariya <pawanharariya@gmail.com>
2020-09-09 22:19:44 +0530

    full functional

---
+++ App/app/src/main/java/com/anvay/customerpanel/Activities/MaidsAlert.java
@@ -0,0 +1,16 @@
+package com.anvay.customerpanel.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anvay.customerpanel.R;
+
+public class MaidsAlert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maids_alert);
+    }
+}
